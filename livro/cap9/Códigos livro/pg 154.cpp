9fc3f4a2f0f5e93f355eeb56a67b42204eb6b35d
arthlz <thurluz06@gmail.com>
2026-03-16 11:58:45 -0300

    Add mais alguns códigos

---
+++ livro/cap9/Códigos livro/pg 154.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string prompt("Enter a line of text: "), line (50, '*');
+
+int main(){
+
+    string text;
+    cout << line << endl << prompt << endl;
+
+    getline(cin, text);
+    cout << line << endl << "Your text is " << text.size()
+         << " characters long!" << endl;
+
+    string copy(text), start(text, 0,10);
+
+    cout << "Your text:\n" << copy << endl;
+    text = "1234567890";
+    cout << line << endl << "The first 10 characters:\n"
+         << start << endl;
+
+    return 0;
+}
---
+++ livro/cap9/Códigos livro/pg 156.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string prompt("Please enter some text: "), line(50, '-');
+
+int main(){
+    prompt += "Terminate the input with an empty line. \n";
+    cout << line << '\n' << prompt << line << endl;
+    string text, line;
+
+    while (true){
+        getline(cin, line);
+        if (line.length() == 0){
+            break;
+        }
+
+        text = line + "\n" + text;
+
+    }
+
+    cout << line << '\n' << "your lines of text in reversed order: "
+         << '\n' << line << endl;
+
+    cout << text << endl;
+
+    return 0;
+}
